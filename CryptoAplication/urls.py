cb590e1d79ec62fba4a6401866b4068c1f4b55e6
Eagle <marcel9817@gmail.com>
2022-04-30 20:42:57 +0200

    1#add_few_pages

---
+++ CryptoAplication/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("wallet/", wallet, name="wallet"),
+    path("graph/", graph, name="graph"),
+]
--- CryptoAplication/views.py
+++ CryptoAplication/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def wallet(request):
+    return render(
+        request,
+        "wallet.html",
+    )
+
+
+def graph(request):
+    return render(
+        request,
+        "graph.html",
+    )
+
+
+def home(request):
+    return render(
+        request,
+        "home.html",
+    )
