707e2565740179f57199227cd6ddaa6420608a12
Eagle <marcel9817@gmail.com>
2022-05-04 20:46:18 +0200

    3#removed_dummy_graph

--- CryptoAplication/views.py
+++ CryptoAplication/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
+
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -19,27 +20,7 @@ def wallet(request):
 
 
 def graph(request):
-    def scatter():
-        x1 = [1, 2, 3, 4]
-        y1 = [30, 35, 25, 45]
-
-        trace = go.Scatter(
-            x=x1,
-            y=y1,
-        )
-        layout = dict(
-            title="Simple Graph",
-            xaxis=dict(range=[min(x1), max(x1)]),
-            yaxis=dict(range=[min(y1), max(y1)]),
-        )
-
-        fig = go.Figure(data=[trace], layout=layout)
-        plot_div = plot(fig, output_type="div", include_plotlyjs=False)
-        return plot_div
-
-    context = {"plot1": scatter()}
-
-    return render(request, "graph.html", context)
+    return render(request, "graph.html")
 
 
 def home(request):
